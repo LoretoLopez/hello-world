e5280484c75801400c99c63c9c16664315d7e2f0
LoretoLopez <145747001+LoretoLopez@users.noreply.github.com>
2023-11-18 10:28:43 +0100

    MAIN

---
+++ prueba.py
@@ -0,0 +1,10 @@
+#Probando para hacer la rama
+
+
+def rellenarDatosCocodrilo(numCoco):
+    listaDientes = []
+    listaLong = []
+    listaPeso = []
+    rellenarDatos(listaDientes,listaLong,listaPeso,numCoco)
+    info = mostrarDatos(listaDientes,listaLong,listaPeso)
+    return info
